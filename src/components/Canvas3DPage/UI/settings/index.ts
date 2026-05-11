3de4eafe335fc48e26b9911a49b39c761b2ee733
kvafex <kvafex2121@gmail.com>
2026-05-12 00:35:43 +0400

    Delete src directory

--- src/components/Canvas3DPage/UI/settings/index.ts
+++
@@ -1,6 +0,0 @@
-import CubeSettings from "./CubeSettings";
-import RingSettings from "./RingSettings";
-import SphereSettings from "./SphereSettings";
-import TorSettings from "./TorSettings";
-
-export { CubeSettings, RingSettings, SphereSettings, TorSettings };
--- src/modules/math3D/entities/index.ts
+++
@@ -1,7 +0,0 @@
-import Point from "./Point";
-import Edge from "./Edge";
-import Polygon from "./Polygon";
-import Figure from "./Figure";
-import Light from "./Light";
-
-export { Point, Edge, Polygon, Figure, Light};
