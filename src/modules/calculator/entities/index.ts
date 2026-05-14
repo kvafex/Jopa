6a90605428d86a41af982e28265f0ebafb8ed01e
kvafex <kvafex2121@gmail.com>
2026-05-15 00:12:30 +0400

    Delete src directory

--- src/modules/calculator/entities/index.ts
+++
@@ -1,7 +0,0 @@
-import Complex from "./Complex";
-import Member from "./Member";
-import Matrix from "./Matrix";
-import Vector from "./Vector";
-import Polynom from "./Polynom";
-
-export { Complex, Member, Polynom, Matrix, Vector};
